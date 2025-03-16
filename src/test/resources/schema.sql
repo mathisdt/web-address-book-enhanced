30f834931634b06c10173d7cd708a357b72dcc06
Mathis Dirksen-Thedens <info@zephyrsoft.org>
2025-03-16 21:51:53 +0100

    make schema generation script compliant with Postgres

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS family (
-id int auto_increment NOT NULL,
+  id int NOT NULL,
   last_name varchar(255) DEFAULT NULL,
   street varchar(255) DEFAULT NULL,
   postal_code varchar(255) DEFAULT NULL,
@@ -8,11 +8,13 @@ id int auto_increment NOT NULL,
   contact2 varchar(255) DEFAULT NULL,
   contact3 varchar(255) DEFAULT NULL,
   remarks varchar(255) DEFAULT NULL,
-  last_update datetime DEFAULT NULL
+  last_update timestamp DEFAULT NULL
 );
 
+ALTER TABLE family ADD PRIMARY KEY (id);
+
 CREATE TABLE IF NOT EXISTS person (
-id int auto_increment NOT NULL,
+  id int NOT NULL,
   first_name varchar(255) DEFAULT NULL,
   last_name varchar(255) DEFAULT NULL,
   birthday varchar(255) DEFAULT NULL,
@@ -21,11 +23,9 @@ id int auto_increment NOT NULL,
   contact3 varchar(255) DEFAULT NULL,
   remarks varchar(255) DEFAULT NULL,
   family_id int DEFAULT NULL,
-  last_update datetime DEFAULT NULL,
+  last_update timestamp DEFAULT NULL,
   ordering int DEFAULT NULL
 );
-
-ALTER TABLE family ADD PRIMARY KEY (id);
 
 ALTER TABLE person ADD PRIMARY KEY (id);
 ALTER TABLE person ADD FOREIGN KEY (family_id) REFERENCES FAMILY;
